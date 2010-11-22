c61c50e46e054981367868911830f9b849dcd9e5
unknown <farnham.ben@gmail.com>
2010-11-22 21:38:30 +0100

    bit more async assertEquals work - pass thru correct values from dll

--- OPCClient/OPCClientToolkitWrapper/AsyncUpdateHandler.cpp
+++ OPCClient/OPCClientToolkitWrapper/AsyncUpdateHandler.cpp
@@ -25,27 +25,23 @@ AsyncUpdateHandler::~AsyncUpdateHandler(void)
 
 void AsyncUpdateHandler::OnDataChange(COPCGroup & group, CAtlMap<COPCItem *, OPCItemData *> & changes)
 {
-	log_NOTICE("OnDataChange called, group [", group.getName(),"] change count [", pantheios::integer(changes.GetCount()),"]");
+	log_NOTICE("OnDataChange called, group [", group.getName(),"] change count [", pantheios::integer(changes.GetCount()),"] have callback [",(callbackFn != NULL?"Y":"N"),"]");
+	
 	for(POSITION pos = changes.GetStartPosition(); pos != NULL; )
 	{
 		CAtlMap<COPCItem *, OPCItemData *>::CPair* pPair = changes.GetNext(pos);
 
-		char buff[nMaxBuffSz];
-		memset(buff, nMaxBuffSz, 0);
-		ConvertOPCItemDataValueToCharArray(*pPair->m_value, buff, nMaxBuffSz);
+		char valueBuff[nMaxBuffSz];
+		memset(valueBuff, nMaxBuffSz, 0);
+		ConvertOPCItemDataValueToCharArray(*pPair->m_value, valueBuff, nMaxBuffSz);
 
-		log_NOTICE("\t item [",pPair->m_key->getName(),"] value [", buff,"]");
-	}
-
-	if(callbackFn != NULL)
-	{
-		log_NOTICE("OnDataChange calling callback fn");
-		this->callbackFn("woo", "hoo");
-		log_NOTICE("OnDataChange called callback fn");
-	}
-	else
-	{
-		log_NOTICE("OnDataChange no callback registered");
+		log_NOTICE("\t item [",pPair->m_key->getName(),"] value [", valueBuff,"]");
+		if(callbackFn != NULL)
+		{
+			log_NOTICE("OnDataChange calling callback fn");
+			callbackFn(pPair->m_key->getName(), valueBuff);
+			log_NOTICE("OnDataChange called callback fn");
+		}
 	}
 }
 
@@ -53,6 +49,5 @@ void AsyncUpdateHandler::SetCallback(updateCallback cb)
 {
 	log_NOTICE("Setting callbackFn - is currently NULL [", (this->callbackFn == NULL?"Y":"N"),"]");
 	this->callbackFn = cb;
-	cb("woo", "hoo");
 	log_NOTICE("Setting callbackFn - is now NULL [", (this->callbackFn == NULL?"Y":"N"),"]");
 }
