c1abc51813db2281855fc8d86641a667436500fa
Ben Farnham <farnham.ben@gmail.com>
2011-08-19 15:18:25 +0200

    Added start.bat containing startup command for end users

---
+++ start.bat
@@ -0,0 +1,4 @@
+REM Developers should read the README.txt then USAGE.txt in this directory before starting
+REM Users should read the USAGE.txt in this directory before starting
+
+java -cp gui-0.1-jar-with-dependencies.jar ch.cern.opc.Main
